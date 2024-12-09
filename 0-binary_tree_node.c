368753fa972fa317e637e04ef38f78836cf0a1e4
Jonas-Jungling <jonas.jungling@oclock.school>
2024-12-09 13:51:08 +0100

    Add error handling for NULL pointer in binary_tree_node function

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,6 +11,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
+	if (newNode == NULL)
+		return (NULL);
+
 	newNode->value = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
